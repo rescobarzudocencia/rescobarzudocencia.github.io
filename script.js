bcc73d920431847219653bd28dd98c450d935080
Rafael Ernesto Escobar Zurita <ernestoescobar@iesincagarcilaso.com>
2026-04-28 17:40:09 +0200

    Menu js aparte

---
+++ script.js
@@ -0,0 +1,15 @@
+const observerOptions = {
+    threshold: 0.1
+};
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+        }
+    });
+}, observerOptions);
+
+document.querySelectorAll('.cycle-section').forEach(section => {
+    observer.observe(section);
+});
